1fcc7cdceb8fcf86002b45ec85732308e4cc1c1b
Tom Collins <tom.collins@moxiworks.com>
2024-04-03 16:31:56 +1100

    user_create_event

--- lib/office365/rest/event.rb
+++ lib/office365/rest/event.rb
@@ -14,9 +14,18 @@ module Office365
       end
 
       def user_events(user_id, args = {})
-        raise Error, "Contact.user_events user id must be supplied" if user_id.nil?
+        raise Error, "Event.user_events user id must be supplied" if user_id.nil?
 
         wrap_results(args.merge(kclass: Models::Event, base_uri: "/users/#{user_id}/calendar/events"))
+      end
+
+      def user_create_event(user_id:, args: {}, params: {})
+        raise Error, "Event.user_create_event user id must be supplied" if user_id.nil?
+
+        response  = post_request(args: args.merge(kclass: Models::Event, base_uri: "/users/#{user_id}/calendar/events"), params: params)
+        {
+          event: Models::Event.new(response)
+        }
       end
     end
   end
